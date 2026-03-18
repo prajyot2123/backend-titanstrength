b3f549e59bfd33b64cbbb688c46d49d68156c53c
Prajyot Deshpande <127429853+prajyot2123@users.noreply.github.com>
2026-03-18 22:10:32 +0530

    Implement cardio controller with CRUD operations

---
+++ controllers/cardio-controller.js
@@ -0,0 +1,53 @@
+const { Cardio, User } = require("../models");
+
+module.exports = {
+  createCardio({ body }, res) {
+    Cardio.create(body)
+      .then((dbCardioData) => {
+        return User.findOneAndUpdate(
+          { _id: body.userId },
+          { $push: { cardio: dbCardioData._id } },
+          { new: true }
+        )
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: "Cardio created but no user with this id!" });
+        }
+        res.json({ message: "Cardio successfully created!" });
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  getCardioById({ params }, res) {
+    Cardio.findOne({ _id: params.id })
+      .then((dbCardioData) => {
+        if (!dbCardioData) {
+          return res.status(404).json({ message: "No cardio data found with this id!" });
+        }
+        res.json(dbCardioData);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteCardio({ params }, res) {
+    Cardio.findOneAndDelete({ _id: params.id })
+      .then((dbCardioData) => {
+        if (!dbCardioData) {
+          res.status(404).json({ message: "No cardio data found with this id!" })
+        }
+        return User.findOneAndUpdate(
+          { cardio: params.id },
+          { $pull: { cardio: params.id } },
+          { new: true }
+        )
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: "Cardio deleted but no user with this id!" });
+        }
+        res.json({ message: "Cardio successfully deleted!" });
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+};
